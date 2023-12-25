f584ad59ebb22d818ebf99c1f609f318cd5426f6
ngyongkang <81545663+ngyongkang@users.noreply.github.com>
2023-12-26 02:30:06 +0800

    Fixed css issue, where it was fixing to the left side of the UI. Had repeated css namespaces on different css files.

--- src/App.jsx
+++ src/App.jsx
@@ -26,15 +26,34 @@ function App() {
     newArray[index] = !newArray[index]
     setApplication(newArray);
   }
-
+  
   return (
     <>
+    {/* Stock template */}
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+    <h1>Vite + React</h1>
+    {/* End of Stock template */}
+
     <div className="card">
     <button onClick={() => onClick(0)}>Calculator</button>
     <button onClick={() => onClick(1)}>Fancy Counter</button>
+  
     </div>
     { applications[0] ? <Calculator /> : null }
     { applications[1] ? <FancyCounter /> : null }
+    <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+    </p>
+    <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+    </p>
     </>
   )
 }
